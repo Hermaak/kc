5583ddc57c462f77ed2d84cbc5616de8ee78718f
sairessy <sairessy@gmail.com>
2026-03-21 08:02:14 +0200

    init

---
+++ js/config.js
@@ -0,0 +1,62 @@
+function formatCurrency(val) {
+  const formatterCurrency = new Intl.NumberFormat("pt-MZ", {
+    style: "decimal",
+    currency: "MZN",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  return formatterCurrency.format(val);
+}
+
+const courses = [
+  {
+    id: 1,
+    label: "Contabilidade + Primavera",
+    month: 3,
+    price: 2997,
+    show: true,
+  },
+  { id: 3, label: "Marketing Digital", month: 3, price: 2997, show: true },
+  {
+    id: 4,
+    label: "Excel básico à avançado",
+    month: 3,
+    price: 2997,
+    show: true,
+  },
+  {
+    id: 6,
+    label: "Recrutamento e selecção",
+    month: 3,
+    price: 2997,
+    show: true,
+  },
+  { id: 7, label: "Atendimento ao cliente", month: 3, price: 2997, show: true },
+  { id: 8, label: "Técnicas de venda", month: 3, price: 2997, show: true },
+  {
+    id: 10,
+    label: "Electricidade instaladora",
+    month: 7,
+    price: 3497,
+    show: true,
+  },
+  {
+    id: 11,
+    label: "Jornalismo, Comunicação e Relações públicas",
+    month: 3,
+    price: 2997,
+    show: true,
+  },
+  { id: 13, label: "Inglês técnico", month: 3, price: 2997, show: true },
+  { id: 14, label: "Redes de computadores", month: 3, price: 2997, show: true },
+  { id: 16, label: "Design", month: 3, price: 2997, show: true },
+  { id: 17, label: "Edição de vídeos", month: 3, price: 2997, show: true },
+  {
+    id: 18,
+    label: "Higiene, Saúde e segurança no trabalho (HSST)",
+    month: 3,
+    price: 2997,
+    show: true,
+  },
+];
---
+++ js/main.js
@@ -0,0 +1,77 @@
+const api_url = window.location.protocol.toLowerCase().includes("s")
+  ? "https://api-master.onrender.com"
+  : "http://localhost:8000";
+
+let menuHidden = true;
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+  $(".menu").animate({ left: menuHidden ? 0 : "-80%" }, 500);
+  menuHidden = !menuHidden;
+});
+
+awakeAPI();
+
+setInterval(async () => {
+  awakeAPI();
+}, 30000);
+
+async function awakeAPI() {
+  try {
+    const res = await fetch(api_url);
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+let cs = "";
+
+for (const c of courses) {
+  if (c.show)
+    cs += `
+     <div class="course">
+        <div class="top">
+          <!--<strong><i class='la la-graduation-cap'></i> Curso</strong>-->
+          <h3>${c.label}</h3>
+          <a href="./details.html?c=${c.id}">Plano do curso <i class='la la-link'></i></a>
+        </div>
+        <div class="bottom">
+          <p>EM APENAS ${c.month} MESES</p>
+          <h2>${formatCurrency(c.price)}</h2>
+          <a href="./signup.html?c=${c.id}">Cadastrar-se</a>
+        </div>
+      </div>
+  `;
+}
+
+document.querySelector(".courses").innerHTML = cs;
+
+document.querySelector("input").addEventListener("input", (e) => {
+  let cs = "";
+  for (const c of courses) {
+    if (
+      c.label.toLowerCase().includes(e.target.value.toLowerCase()) &&
+      c.show
+    ) {
+      cs += `
+      <div class="course">
+        <div class="top">
+          <!--<strong><i class='la la-graduation-cap'></i> Curso</strong>-->
+          <h3>${c.label}</h3>
+          <a href="./etails.html?c=${c.id}">Plano do curso <i class='la la-link'></i></a>
+        </div>
+        <div class="bottom">
+          <p>EM APENAS ${c.month} MESES</p>
+          <h2>${formatCurrency(c.price)}</h2>
+          <a href="./signup.html?c=${c.id}">Cadastrar-se</a>
+        </div>
+      </div>
+    `;
+    }
+  }
+
+  document.querySelector(".courses").innerHTML = cs;
+});
